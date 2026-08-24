bcc09db2895b90538e6faad4188186bc3980ff1b
huangruiteng <huangrt01@163.com>
2026-08-25 00:08:33 +0800

    Project repository delivery through capability hooks (#3570)
    * feat(change-window): project repository delivery through hooks
    
    Signed-off-by: huangruiteng <huangrt01@163.com>
    
    * test(change-window): cover hook projection and legacy guards
    
    Signed-off-by: huangruiteng <huangrt01@163.com>
    
    * docs(change-window): document hook-owned delivery projection
    
    Signed-off-by: huangruiteng <huangrt01@163.com>
    
    ---------
    
    Signed-off-by: huangruiteng <huangrt01@163.com>

--- loopx/control_plane/effect_runtime_handlers.ts
+++ loopx/control_plane/effect_runtime_handlers.ts
@@ -74,6 +74,10 @@ import {
   projectQuotaActionPortfolio,
   qualifyActionSelection,
 } from "./work_items/action_portfolio.ts";
+import {
+  validateInteractionProjectionHookInvocation,
+  validateInteractionProjectionHookRegistration,
+} from "./capability_hooks.ts";
 
 type EffectRuntimeHandler = (params: JsonObject) => unknown | Promise<unknown>;
 
@@ -333,6 +337,19 @@ export function createEffectRuntimeHandlers(
     [
       "governed_capability.settlement_status",
       (params) => governedCapabilitySettlementStatus(params.failure),
+    ],
+    [
+      "capability_hook.interaction_projection.validate_registration",
+      (params) => validateInteractionProjectionHookRegistration(
+        params.registration,
+      ),
+    ],
+    [
+      "capability_hook.interaction_projection.validate",
+      (params) => validateInteractionProjectionHookInvocation({
+        registration: params.registration,
+        result: params.result,
+      }),
     ],
     [
       "settlement.identity",
